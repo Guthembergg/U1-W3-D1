9b564a05a10f320653df1bca7de8d2dbae37822f
Karim Guettech <karimguettech@gmail.com>
2022-12-12 13:27:07 +0100

    primi 7 esercizi fatti

--- D6.js
+++ D6.js
@@ -13,6 +13,10 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function area(l1, l2) {
+  console.log("l'area è:", l1 * l2);
+}
+area(3, 2);
 /* ESERCIZIO 2
  Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
  La funzione deve ritornare la somma dei due parametri, ma se il valore dei due parametri è il medesimo deve invece tornare
@@ -21,12 +25,33 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function creazySum(num1, num2) {
+  numsomma = num1 + num2;
+  if (num1 === num2) {
+    console.log(numsomma * 3);
+  } else {
+    console.log(numsomma);
+  }
+}
+creazySum(2, 2);
+
 /* ESERCIZIO 3
  Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
  Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+function creazyDiff(num1) {
+  numdiff = num1 - 19;
+  Math.abs(numdiff);
+  if (num1 > 19) {
+    console.log(Math.abs(numdiff) * 3);
+  } else {
+    console.log(Math.abs(numdiff));
+  }
+}
+creazyDiff(20);
 
 /* ESERCIZIO 4
  Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
@@ -35,6 +60,17 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function boundary(n) {
+  if (typeof n === "number" && Number.isInteger(n)) {
+    if ((n > 20 && n <= 100) || n === 400) {
+      return true;
+    }
+  } else {
+    return "inserisci numero intero";
+  }
+}
+console.log(boundary(500.5));
+
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
  La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
@@ -42,6 +78,19 @@ REGOLE
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+function epify(string) {
+  if (typeof string === "string") {
+    epicode = "EPICODE ";
+    if (string.substr(0, 7) === "EPICODE") {
+      console.log(string);
+    } else {
+      console.log(epicode.concat(string));
+    }
+  } else {
+    console.log("inserisci una stringa");
+  }
+}
+epify("ciao");
 
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
@@ -50,11 +99,34 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function check3and7(n) {
+  if (typeof n === "number" && n > 0) {
+    if (n % 3 === 0) {
+      console.log("divisibile per tre");
+    } else {
+      console.log("non divisibile per tre");
+    }
+    if (n % 7 === 0) {
+      console.log("divisibile per sette");
+    } else {
+      console.log("non divisibile per sette");
+    }
+  }
+}
+check3and7(6);
 /* ESERCIZIO 7
  Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+function reverseString(string) {
+  if (typeof string === "string") {
+    console.log(string.split("").reverse().join(""));
+  } else {
+    console.log("inserisci stringa");
+  }
+}
+reverseString("ciao amico");
 
 /* ESERCIZIO 8
  Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
@@ -63,6 +135,19 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+function upperFirst(string) {
+  arraystring = string.split(" ");
+  console.log(arraystring);
+  let arraym = [];
+  let maiusc = 0;
+  for (let i = 0; i < arraystring; i++) {
+    maiusc = arraystring[i].substring(0, 1).toUpperCase();
+    console.log(maiusc);
+    arraym = arraym.push(maiusc);
+  }
+  console.log(arraym);
+}
+upperFirst("ciao amici");
 /* ESERCIZIO 9
  Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
  della stringa originale.
